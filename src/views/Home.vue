9fe3f43d63269ed9a42b20e740e950f1f903a78b
Chenic <celiachenic@gmail.com>
2021-09-02 11:59:52 +0800

    環境設置

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>jjwef</h1>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  mounted() {
+    console.log(process.env.VUE_APP_API, process.env.VUE_APP_PATH);
+  },
+};
 </script>
+<style lang="scss" scoped>
+
+</style>
