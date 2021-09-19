ecbadde18b75da0fd59d9527b497ba442d4ff94b
Chenic <celiachenic@gmail.com>
2021-09-19 20:03:33 +0800

    引入font 首頁基本完成
    (cherry picked from commit a343ec681fa4b83c065b301e0c893d195b9b0fb8)

--- src/views/buyer/userboard/home/VideoAd.vue
+++ src/views/buyer/userboard/home/VideoAd.vue
@@ -1,9 +1,24 @@
 <template>
   <div class="position-relative">
-    <div class="position-absolute top-50 start-50 translate-middle">
-      <h1 class="title text-white text-center">
+    <div class="content position-absolute top-50 start-50 translate-middle">
+      <h1 class="eng fw-lighter text-white text-center">
         Make your next trip awesome
       </h1>
+      <router-link
+        to="/store/about"
+        class="d-block text-white fw-lighter text-center text-decoration-none mt-4"
+        >越南簡介</router-link
+      >
+      <router-link
+        to="/store/about"
+        class="d-block text-white fw-lighter text-center text-decoration-none mt-2"
+        >關於我們</router-link
+      >
+      <router-link
+        to="/store/products"
+        class="d-block text-white fw-lighter text-center text-decoration-none mt-2"
+        >查看行程</router-link
+      >
     </div>
     <div class="d-xl-none">
       <video autoplay muted loop class="mobile vw-100 vh-100">
@@ -26,11 +41,27 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
-h1 {
-  padding: 10px;
-  box-shadow: 0 0 20px 10px rgb(250, 247, 247);
+.content {
+  z-index: 100;
+  h1 {
+    padding: 10px;
+    box-shadow: 0 0 20px 10px rgb(250, 247, 247);
+  }
+  a:hover {
+    animation: text 0.5s infinite alternate;
+  }
 }
+
 video {
   object-fit: cover;
+}
+
+@keyframes text {
+  0% {
+    transform: translateY(4px);
+  }
+  100% {
+    transform: translateY(-4px);
+  }
 }
 </style>
