28f15475d25f2148aed3823d79b026b5bb6c28dc
Chenic <celiachenic@gmail.com>
2021-09-23 11:47:46 +0800

    購物車基本樣式
    (cherry picked from commit 0ae13f4dcd6f24467fec434720251b7b14c42104)

--- src/views/buyer/userboard/cart/Cart.vue
+++ src/views/buyer/userboard/cart/Cart.vue
@@ -1,49 +1,90 @@
 <template>
   <div>
     <loading :active="isLoading"></loading>
-    <div class="container-fluid">
-      <h1>購物車</h1>
-      <table class="table align-middle">
-        <thead>
-          <tr>
-            <th scope="col">商品</th>
-            <th scope="col">單價</th>
-            <th scope="col">數量</th>
-            <th scope="col"></th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="item in cartItems" :key="item.id">
-            <td @click="backToProduct(item.product_id)">
-              {{ item.product.title }}
-            </td>
-            <td>{{ item.product.price }}</td>
-            <td>
-              <div class="input-group">
-                <input
-                  v-model="item.qty"
-                  @change="updateCart(item.id, item.qty)"
-                  min="1"
-                  type="number"
-                  class="form-control"
-                  placeholder="數量"
-                  aria-label="Username"
-                  aria-describedby="basic-addon1"
-                />
-                <span class="input-group-text" id="basic-addon1">{{
-                  item.product.unit
-                }}</span>
-              </div>
-            </td>
-            <td>
-              <button @click="deleteCart(item.id)" class="btn text-danger">
-                <i class="bi bi-cart-dash-fill"></i>
-              </button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-      <h3 class="text-end">總計：NTD {{ total.toLocaleString() }} 元</h3>
+    <div class="container mt-4">
+      <h1 class="text-center">購物車</h1>
+      <div class="d-flex justify-content-center mt-4">
+        <table class="table align-middle table-fit">
+          <thead>
+            <tr>
+              <th scope="col">商品</th>
+              <th scope="col">單價</th>
+              <th scope="col">數量</th>
+              <th scope="col">刪除</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="item in cartItems" :key="item.id">
+              <td @click="backToProduct(item.product_id)">
+                {{ item.product.title }}
+              </td>
+              <td>{{ item.product.price }}</td>
+              <td>
+                <div class="input-group">
+                  <input
+                    v-model="item.qty"
+                    @change="updateCart(item.id, item.qty)"
+                    min="1"
+                    type="number"
+                    class="form-control"
+                    placeholder="數量"
+                    aria-label="Username"
+                    aria-describedby="basic-addon1"
+                  />
+                  <span class="input-group-text" id="basic-addon1">{{
+                    item.product.unit
+                  }}</span>
+                </div>
+              </td>
+              <td>
+                <button @click="deleteCart(item.id)" class="btn text-danger">
+                  <i class="bi bi-cart-dash-fill"></i>
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td class="no-border" colspan="4">
+                <div class="input-group">
+                  <input
+                    type="text"
+                    class="form-control"
+                    placeholder="輸入優惠券"
+                    aria-label="Recipient's username"
+                    aria-describedby="button-addon2"
+                  />
+                  <button
+                    class="btn btn-outline-secondary"
+                    type="button"
+                    id="button-addon2"
+                  >
+                    使用優惠券
+                  </button>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td class="no-border" colspan="4">
+                <h3
+                  class="text-end"
+                  :class="{
+                    'text-decoration-line-through': useCode,
+                    'text-secondary': useCode,
+                  }"
+                >
+                  總計：NT {{ total.toLocaleString() }} 元
+                </h3>
+              </td>
+            </tr>
+            <tr v-show="this.useCode">
+              <td class="no-border" colspan="4">
+                <h3 class="text-end">
+                  折扣後：NT {{ final_total.toLocaleString() }} 元
+                </h3>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   </div>
 </template>
@@ -56,6 +97,7 @@ export default {
       total: 0,
       final_total: 0,
       isLoading: false,
+      useCode: false,
     };
   },
   methods: {
@@ -101,4 +143,20 @@ export default {
   },
 };
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+table.table-fit {
+  width: auto !important;
+  table-layout: auto !important;
+
+  thead th,
+  tfoot th {
+    width: auto !important;
+  }
+  tbody td {
+    width: auto !important;
+    &.no-border {
+      border-bottom: none;
+    }
+  }
+}
+</style>
