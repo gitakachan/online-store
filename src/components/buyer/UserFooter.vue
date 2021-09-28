ec3ce537c829bf0df1c69d9a3b73ffc4db088458
Chenic <celiachenic@gmail.com>
2021-09-28 10:32:53 +0800

    加入qrCode, 調整About.vue樣式

--- src/components/buyer/UserFooter.vue
+++ src/components/buyer/UserFooter.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="bg-light">
     <div class="container py-5">
-      <div class="row">
-        <div class="col-12">
-          <div class="name d-flex flex-row align-items-center mb-4">
+      <div class="row d-flex">
+        <div class="col-12 order-md-1">
+          <div
+            class="name d-flex flex-row align-items-center mb-4 justify-content-center justify-content-md-start"
+          >
             <div class="icon">
               <img class="img-fluid" src="@/assets/imgs/icon.png" alt="" />
             </div>
@@ -12,8 +14,38 @@
             </h4>
           </div>
         </div>
-        <div class="col-md-6">
-          <div class="base">
+
+        <div class="col-md-4 order-md-3">
+          <div
+            class="follow mb-5 d-flex flex-column align-items align-items-md-center"
+          >
+            <h5 class="mb-3 fw-light mx-auto">追蹤我們</h5>
+            <div class="links mx-auto">
+              <a
+                href="https://twitter.com/?lang=zh-tw"
+                target="_blank"
+                class="m-3"
+                ><i class="bi bi-twitter text-info"></i
+              ></a>
+              <a href="https://zh-tw.facebook.com/" target="_blank" class="m-3"
+                ><i class="bi bi-facebook fb"></i
+              ></a>
+              <a href="https://www.instagram.com/" target="_blank" class="m-3"
+                ><i class="bi bi-instagram ig"></i
+              ></a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4 order-md-4">
+          <div class="qrcode mb-5 d-flex flex-column align-items-md-center">
+            <h5 class="mb-3 fw-light mx-auto">即時諮詢</h5>
+            <img class="mx-auto" src="@/assets/imgs/vnextQRCode.svg" alt="" />
+          </div>
+        </div>
+
+        <div class="col-12 col-md-4 order-md-2">
+          <div class="base mb-5 d-flex flex-column align-items-center">
             <h5 class="mb-3 fw-light">服務據點</h5>
             <ul class="eng list-unstyled fw-light">
               <li>台北 - TAIPEI</li>
@@ -23,29 +55,9 @@
             </ul>
           </div>
         </div>
-        <div class="col-md-6">
-          <div class="follow mb-3">
-            <h5 class="mb-3 fw-light">追蹤我們</h5>
-            <a href="#" target="_blank" class="m-3"
-              ><i class="bi bi-twitter text-info"></i
-            ></a>
-            <a href="#" target="_blank" class="m-3"
-              ><i class="bi bi-facebook fb"></i
-            ></a>
-            <a href="#" target="_blank" class="m-3"
-              ><i class="bi bi-instagram ig"></i
-            ></a>
-          </div>
-        </div>
-        <div class="col-12 fw-light">
-          <p class="my-2">
+        <div class="col-12 fw-light order-md-5 d-flex flex-column">
+          <p class="my-2 mx-auto">
             &copy; 本網站為學習與練習用途
-            <a
-              href="https://www.linkedin.com/in/chenichen"
-              target="_blank"
-              class="m-1"
-              ><i class="bi bi-linkedin fb"></i
-            ></a>
             <a
               href="https://github.com/gitakachan/online-store"
               target="_blank"
@@ -53,7 +65,7 @@
               ><i class="bi bi-github text-dark"></i
             ></a>
           </p>
-          <p>
+          <p class="mx-auto">
             影片素材：Stock footage provided by Videvo, downloaded from
             <a href="https://www.videvo.net" target="_blank" class="text-info"
               >www.videvo.net</a
@@ -84,6 +96,10 @@ export default {
     li {
       letter-spacing: 3px;
     }
+  }
+
+  .qrcode img {
+    max-width: 120px;
   }
 }
 </style>
